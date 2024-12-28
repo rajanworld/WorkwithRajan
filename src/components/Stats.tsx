b0ccb82553b91349aacfbdd7abc57f03440adbd2
rajanworld <rjsolutions001@gmail.com>
2024-12-28 16:28:42 +0530

    update stats

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -5,14 +5,14 @@ const Stats = () => {
         <div className="grid md:grid-cols-3 gap-8 text-center">
           <div className="space-y-2">
             <p className="text-4xl font-bold text-mint">$100K+</p>
-            <p className="text-white/80">Total Earnings</p>
+            <p className="text-white/80">Earnings</p>
           </div>
           <div className="space-y-2">
-            <p className="text-4xl font-bold text-mint">124</p>
+            <p className="text-4xl font-bold text-mint">125</p>
             <p className="text-white/80">Total Jobs</p>
           </div>
           <div className="space-y-2">
-            <p className="text-4xl font-bold text-mint">2,124</p>
+            <p className="text-4xl font-bold text-mint">2,000+</p>
             <p className="text-white/80">Total Hours</p>
           </div>
         </div>
